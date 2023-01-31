ed612b1f313bc3a5dceb4b59d0e8c9a23b348cfe
rorycreedon <rory.s.creedon@gmail.com>
2023-01-31 15:16:42 +0000

    Setting up loop to download data

--- download_data.py
+++ download_data.py
@@ -11,10 +11,12 @@ import lzma
 import os
 import shutil
 from pathlib import Path
+from tqdm import tqdm
 
 ####################################################
 # DOWNLOADING DATA
 ####################################################
+
 
 def download_extract_data(url, name):
 	"""
@@ -28,13 +30,15 @@ def download_extract_data(url, name):
 	# Download and unzip files from website
 	response = requests.get(url)
 	with zipfile.ZipFile(BytesIO(response.content)) as zip_ref:
-		zip_ref.extractall('temp/')
+		zip_ref.extractall("temp/")
 
 	# Get name of new folder made
 	folder = next(os.walk("temp/"))[1][0]
 
 	# Extract .jsonl file from .xz file
-	with lzma.open("temp/" + folder + "/data/data.jsonl.xz") as f, open('temp/' + name + '.jsonl', 'wb') as f_out:
+	with lzma.open("temp/" + folder + "/data/data.jsonl.xz") as f, open(
+		"temp/" + name + ".jsonl", "wb"
+	) as f_out:
 		file_content = f.read()
 		f_out.write(file_content)
 
@@ -51,7 +55,7 @@ def jsonl_to_df(name):
 	"""
 
 	# Convert data from .jsonl to dataframe
-	with jsonlines.open('temp/' + name + '.jsonl', 'r') as jsonl_f:
+	with jsonlines.open("temp/" + name + ".jsonl", "r") as jsonl_f:
 		lst = [obj for obj in jsonl_f]
 	df = pd.json_normalize(lst)
 
@@ -60,7 +64,9 @@ def jsonl_to_df(name):
 		temp_df = df[column].apply(pd.Series)
 		for i in range(len(temp_df.columns)):
 			df_to_concat = pd.json_normalize(temp_df[i])
-			df_to_concat.columns = [column + "." + str(col) + str(i+1) for col in df_to_concat.columns]
+			df_to_concat.columns = [
+				column + "." + str(col) + str(i + 1) for col in df_to_concat.columns
+			]
 			df = df.join(df_to_concat)
 
 		df = df.drop([column], axis=1)
@@ -68,21 +74,26 @@ def jsonl_to_df(name):
 		return df
 
 	# Fixing list of dicts
-	df = fix_list_of_dicts('casebody.data.opinions', df)
-	df = fix_list_of_dicts('citations', df)
+	df = fix_list_of_dicts("casebody.data.opinions", df)
+	df = fix_list_of_dicts("citations", df)
 
 	# Delete .jsonl file
-	os.remove('temp/' + name + '.jsonl')
+	os.remove("temp/" + name + ".jsonl")
 
-	return df
+	df.to_pickle("data/" + name + ".pkl")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
 	# Make folders if necessary
 	Path("data/").mkdir(parents=True, exist_ok=True)
 	Path("temp/").mkdir(parents=True, exist_ok=True)
 
-	# Download New Mexico data
-	url = "https://case.law/download/bulk_exports/latest/by_jurisdiction/case_text_open/nm/nm_text.zip"
-	new_mexico_jsonl = download_extract_data(url, "nm_extracted")
-	new_mexico_df = jsonl_to_df("nm_extracted").to_pickle("data/new_mexico.pkl")
+	# Links and relevant states to download
+	urls = ["https://case.law/download/bulk_exports/latest/by_jurisdiction/case_text_open/nm/nm_text.zip", "https://case.law/download/bulk_exports/latest/by_jurisdiction/case_text_open/ark/ark_text.zip"]
+	states = ["new_mexico", "arkansas"]
+
+	# Downloading and saving data
+	for i in tqdm(range(len(urls))):
+		download_extract_data(urls[i], states[i])
+		jsonl_to_df(states[i])
