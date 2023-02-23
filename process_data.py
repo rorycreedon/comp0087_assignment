865cf54788f0cb625033b6d0ca31bf02f7b5222f
Tik Nang Tsoi (Tristan) <97603154+tikantsoi@users.noreply.github.com>
2023-02-23 23:21:08 +0000

    process ECHR data

--- process_data.py
+++ process_data.py
@@ -1,17 +1,31 @@
+#%% Imports
 import pandas as pd
+import json
+import glob
 
-#%%
-
-def open_pickle(name):
+# %%
+def open_json(path):
     """
-    Load pickle file into a dataframe
+    Load and concatenate json files in a directory
     """
-    with open('data/' + name + '.pkl', 'rb') as file:
-        df = pd.read_pickle(file)
-        df.head
+    # create an empty list to store the DataFrames
+    df_list = []
 
-    return
+    for file_path in glob.glob("data/"+path+"/*.json"):
+        with open(file_path, 'r') as f:
+            # load the json data from the file object using the json module
+            json_obj = json.load(f)
+        # convert the json object to a pandas DataFrame
+        df = pd.json_normalize(json_obj)
+        # append the DataFrame to the list
+        df_list.append(df)
 
-open_pickle("new_mexico")
+    # concatenate all of the DataFrames in the list
+    result_df = pd.concat(df_list, axis=0, ignore_index=True)
+
+    return result_df
+
+# %%
+df = open_json("ECHR_Dataset/EN_train")
 
 # %%
