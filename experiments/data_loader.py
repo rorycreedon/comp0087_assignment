c6348bad9550ab27f3183326380fafb203bad946
Charlie Brown <charliebrown@eduroam-int-dhcp-97-240-138.ucl.ac.uk>
2023-03-20 15:21:51 +0000

    Added chunking

--- experiments/data_loader.py
+++ experiments/data_loader.py
@@ -5,6 +5,7 @@ import packages
 import pandas as pd
 from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
 import torch
+from chunking import chunk_text
 
 """
 dictionaries
@@ -64,6 +65,59 @@ def load_data(folder="echr", task="binary_cls", anon=False):
 
     return train_texts, train_labels, val_texts, val_labels, test_texts, test_labels
 
+def chunking(folder="echr", anon=False):
+    
+    """
+    Generates chunked data for the ECHR dataset.
+
+    Args:
+        folder (str, optional): Name of the data folder. Defaults to "echr".
+        anon (bool, optional): Include anon data or not. Defaults to False.
+
+    Returns:
+        new_df (pandas dataframe): New dataframe with chunked texts.
+    """
+    
+    # load data
+    if anon == False:
+        train_df = pd.read_pickle(f"data/{folder}/non-anon_train.pkl")
+        val_df = pd.read_pickle(f"data/{folder}/non-anon_valid.pkl")
+        test_df = pd.read_pickle(f"data/{folder}/non-anon_test.pkl")
+    else:
+        train_df = pd.read_pickle(f"data/{folder}/anon_train.pkl")
+        val_df = pd.read_pickle(f"data/{folder}/anon_valid.pkl")
+        test_df = pd.read_pickle(f"data/{folder}/anon_test.pkl")
+
+    if folder == "echr":
+        text_column = "text"
+    else:
+        text_column = "summary"
+        
+    # create new dataframe for each chunk
+    new_df = train_df.drop('text', axis=1)
+    index_of_text_column = new_df.columns.get_loc(new_df.columns[0])
+    new_df.insert(index_of_text_column + 1, 'chunks', '')
+    
+    # chunk texts and store in new dataframe    
+    for i, text in enumerate(train_df["text"].tolist()):
+        
+        # chunk text
+        chunks = chunk_text(text)
+        
+        # create new rows for each chunk
+        row_to_copy = new_df.iloc[i]
+        new_rows = pd.concat([row_to_copy]*len(chunks), axis=1).transpose()
+        new_df = pd.concat([new_df, new_rows], ignore_index=True)
+        
+        # store chunks in new dataframe
+        for j, chunk in enumerate(chunks):
+            new_df['chunks'][len(new_df) - len(chunks) + j] = chunk
+            
+    # remove first 7100 rows
+    new_df = new_df.iloc[7100:]
+    
+    return new_df
+        
 # tokenize texts
 def generate_tokens(tokenizer, texts, max_length=512):
     tokens = tokenizer.batch_encode_plus(texts, 
