d3554033ec2bce5d6949f894680c18133b6226c9
Tik Nang Tsoi (Tristan) <97603154+tikantsoi@users.noreply.github.com>
2023-04-02 18:13:26 +0100

    Cleaned repository

--- experiments/get_loss.py
+++ experiments/get_loss.py
@@ -9,6 +9,12 @@ functions
 """
 
 def get_loss_func(task):
+    """
+    Get the loss function based on the task.
+
+    Params:
+    `task` (str): the task to be performed
+    """
     if task == "binary_cls" or task == "multi_cls":
         loss_func = nn.BCELoss()
     elif task == "regression":
